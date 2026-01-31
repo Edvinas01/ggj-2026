33173ba178bba067782ff29fd7c6a105bc933e00
Edvinas Danevičius <edvinas108@gmail.com>
2026-01-31 03:59:47 +0200

    Add item actor and interaction with items

---
+++ Assets/Scripts/Runtime/Items/ItemActor.cs
@@ -0,0 +1,51 @@
+﻿using RIEVES.GGJ2026.Core.Interaction.Interactables;
+using UnityEngine;
+
+namespace RIEVES.GGJ2026.Runtime.Items
+{
+    internal sealed class ItemActor : MonoBehaviour
+    {
+        [Header("General")]
+        [SerializeField]
+        private ItemData data;
+
+        [SerializeField]
+        private Interactable interactable;
+
+        private void OnEnable()
+        {
+            interactable.OnHoverEntered += OnInteractableHoverEntered;
+            interactable.OnHoverExited += OnInteractableHoverExited;
+            interactable.OnSelectEntered += OnInteractableSelectEntered;
+            interactable.OnSelectExited += OnInteractableSelectExited;
+        }
+
+        private void OnDisable()
+        {
+            interactable.OnHoverEntered -= OnInteractableHoverEntered;
+            interactable.OnHoverExited -= OnInteractableHoverExited;
+            interactable.OnSelectEntered -= OnInteractableSelectEntered;
+            interactable.OnSelectExited -= OnInteractableSelectExited;
+        }
+
+        private void OnInteractableHoverEntered(InteractableHoverEnteredArgs args)
+        {
+            Debug.Log($"Item hover entered {name}", this);
+        }
+
+        private void OnInteractableHoverExited(InteractableHoverExitedArgs args)
+        {
+            Debug.Log($"Item hover exited {name}", this);
+        }
+
+        private void OnInteractableSelectEntered(InteractableSelectEnteredArgs args)
+        {
+            Debug.Log($"Item select entered {name}", this);
+        }
+
+        private void OnInteractableSelectExited(InteractableSelectExitedArgs args)
+        {
+            Debug.Log($"Item select exited {name}", this);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Items/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using RIEVES.GGJ2026.Core.Constants;
+using UnityEngine;
+
+namespace RIEVES.GGJ2026.Runtime.Items
+{
+    [CreateAssetMenu(
+        fileName = CreateAssetMenuConstants.BaseFileName + "Data_Item",
+        menuName = CreateAssetMenuConstants.BaseMenuName + "/Item Data",
+        order = CreateAssetMenuConstants.BaseOrder
+    )]
+    internal sealed class ItemData : ScriptableObject
+    {
+    }
+}
