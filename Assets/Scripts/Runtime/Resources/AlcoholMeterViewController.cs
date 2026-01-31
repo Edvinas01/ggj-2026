aaff1e5631fddac6c710441b4029617ad0ba219d
Edvinas Danevičius <edvinas108@gmail.com>
2026-01-31 05:40:28 +0200

    Add alko meter

---
+++ Assets/Scripts/Runtime/Resources/AlcoholMeterViewController.cs
@@ -0,0 +1,37 @@
+﻿using RIEVES.GGJ2026.Core.Views;
+using UnityEngine;
+
+namespace RIEVES.GGJ2026.Runtime.Resources
+{
+    internal sealed class AlcoholMeterViewController : ViewController<AlcoholMeterView>
+    {
+        [SerializeField]
+        private ResourceController resourceController;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            View.Fill = resourceController.AlcoholRatio;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            resourceController.OnAlcoholChanged += OnAlcoholChanged;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            resourceController.OnAlcoholChanged -= OnAlcoholChanged;
+        }
+
+        private void OnAlcoholChanged(AlcoholChangedArgs args)
+        {
+            View.Fill = args.Ratio;
+        }
+    }
+}
