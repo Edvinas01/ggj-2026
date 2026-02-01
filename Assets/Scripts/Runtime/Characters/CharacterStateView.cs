88cb6fa7225e0093bcdc54ea14a19ea9719ec576
Edvinas Danevičius <edvinas108@gmail.com>
2026-02-01 10:44:35 +0200

    Add debug view for chars

---
+++ Assets/Scripts/Runtime/Characters/CharacterStateView.cs
@@ -0,0 +1,17 @@
+﻿using RIEVES.GGJ2026.Core.Views;
+using TMPro;
+using UnityEngine;
+
+namespace RIEVES.GGJ2026.Runtime.Characters
+{
+    internal sealed class CharacterStateView : View
+    {
+        [SerializeField]
+        private TMP_Text debugInfoText;
+
+        public string DebugInfoText
+        {
+            set => debugInfoText.text = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Characters/CharacterStateViewController.cs
@@ -0,0 +1,33 @@
+﻿using RIEVES.GGJ2026.Core.Views;
+using UnityEngine;
+
+namespace RIEVES.GGJ2026.Runtime.Characters
+{
+    internal sealed class CharacterStateViewController : ViewController<CharacterStateView>
+    {
+        [SerializeField]
+        private CharacterActor character;
+
+        [SerializeField]
+        private bool isDebug;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (isDebug == false)
+            {
+                return;
+            }
+
+            var text = "";
+            var poi = character.CurrentTarget ? character.CurrentTarget.name : null;
+            text += $"\nCurrentAnimationState: <color=red>{character.CurrentAnimationState}</color>";
+            text += $"\nCurrentState: <color=red>{character.CurrentState}</color>";
+            text += $"\nCurrentActivity: <color=red>{character.CurrentActivity}</color>";
+            text += $"\nPOI: <color=red>{poi}</color>";
+
+            View.DebugInfoText = text;
+        }
+    }
+}
